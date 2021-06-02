13d606be75688e705446bb8044b9b74e8c99dcb5
Stephen Marold <smmarold@email.neit.edu>
2021-06-02 11:16:39 -0400

    TTT Update

---
+++ se245/TicTacToe/GameController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+	class GameController
+	{
+		public List<string> positions = new();
+		
+		//Build's the game board. Uses the positions list (initialized to spaces in the constructor) to replace with X and O accordingly. 
+		private void BuildBoard()
+		{
+			Console.WriteLine($" " + positions[6] + " " + "|" + " " + positions[7] + " " + "|" + " " + positions[8] + " ");
+			Console.WriteLine("---+---+---"); 								
+			Console.WriteLine($" " + positions[3] + " " + "|" + " " + positions[4] + " " + "|" + " " + positions[5] + " ");
+			Console.WriteLine("---+---+---"); 					
+			Console.WriteLine($" " + positions[0] + " " + "|" + " " + positions[1] + " " + "|" + " " + positions[2] + " ");
+		}
+
+		//Takes the Player Turn String (X or O) an puts that in the appropriate spot in the position.
+		public void UpdateBoard(string playerLetter, string position)
+		{
+			switch (position)
+			{
+				case "1":
+					positions[0] = playerLetter;
+					break;
+				case "2":
+					positions[1] = playerLetter;
+					break;
+				case "3":
+					positions[2] = playerLetter;
+					break;
+				case "4":
+					positions[3] = playerLetter;
+					break;
+				case "5":
+					positions[4] = playerLetter;
+					break;
+				case "6":
+					positions[5] = playerLetter;
+					break;
+				case "7":
+					positions[6] = playerLetter;
+					break;
+				case "8":
+					positions[7] = playerLetter;
+					break;
+				case "9":
+					positions[8] = playerLetter;
+					break;
+			}
+
+			//Clearing the console and calling BuildBoard (which will now have the updated positions in the List)
+			Console.Clear();
+			BuildBoard();
+		}
+
+		//Takes Player (X or O) and checks the various win states of the game against that letter. 
+		//If no player has one, checks for a tie by checking the List for spaces (no spaces = tie)
+		public bool IsGameOver(string playerSign)
+		{
+			bool gameOver = false;
+
+			//Normally I avoid single lin ifs. But there was a lot of them, and I wanted it to be clean. 
+			if (positions[0] == playerSign && positions[1] == playerSign && positions[2] == playerSign) gameOver = true;
+			if (positions[3] == playerSign && positions[4] == playerSign && positions[5] == playerSign) gameOver = true;
+			if (positions[6] == playerSign && positions[7] == playerSign && positions[8] == playerSign) gameOver = true;
+			if (positions[0] == playerSign && positions[3] == playerSign && positions[6] == playerSign) gameOver = true;
+			if (positions[1] == playerSign && positions[4] == playerSign && positions[7] == playerSign) gameOver = true;
+			if (positions[2] == playerSign && positions[5] == playerSign && positions[8] == playerSign) gameOver = true;
+			if (positions[0] == playerSign && positions[4] == playerSign && positions[8] == playerSign) gameOver = true;
+			if (positions[2] == playerSign && positions[4] == playerSign && positions[6] == playerSign) gameOver = true;
+
+			if (gameOver)
+				Console.WriteLine($"Player {playerSign} Wins!");
+
+			if (!positions.Contains(" ") && !gameOver)
+			{
+				gameOver = true;
+				Console.WriteLine("The Game is a Tie!");
+			}
+
+			return gameOver;
+		}
+
+		//On creation, Initialize our positions list to a space, and builds the first board
+		public GameController()
+		{
+			for (int i = 0; i < 9; i++)
+			{
+				positions.Add(" ");
+			}
+			BuildBoard();
+		}
+	}
+}
---
+++ se245/TicTacToe/Player.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+	class Player
+	{
+		List<string> chosenPositions = new();
+		
+		//Seems simple, but I wanted anything related to the Player in the player class. 
+		//Takes the current player and swaps it, returning the opposite player
+		public string PlayerTurn(string playerLetter)
+		{
+			if (playerLetter.Contains("X"))
+				playerLetter = "O";
+			else
+				playerLetter = "X";
+
+			return playerLetter;
+		}
+
+		public string GetPlayerChoice()
+		{
+			//Set up CKI for reading key, choice var, and while loop var
+			ConsoleKeyInfo cki;
+			string playerChoice = "";
+			bool validChoice = false;
+
+			while (!validChoice)
+			{
+				//get the key from the player
+				cki = Console.ReadKey();
+				playerChoice = cki.Key.ToString().Substring(cki.Key.ToString().Length - 1, 1);
+
+				//If it is NOT a digit and NOT the Esc Key, or if it's already in our choices made List, error out, try again
+				if ((!Char.IsDigit(playerChoice[0]) && cki.Key != ConsoleKey.Escape) || chosenPositions.Contains(playerChoice))
+				{
+					Console.WriteLine("\nInvalid Selection. Try Again.");
+				}
+				else if (Char.IsDigit(playerChoice[0])) //if digit, add to selections, break out of loop
+				{
+					chosenPositions.Add(playerChoice);
+					validChoice = true;
+
+				}
+				else
+				{
+					validChoice = true; //if escape, break out of loop
+				}
+			}
+
+			return playerChoice;
+		}
+	}
+}
