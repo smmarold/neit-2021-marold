6fffb60d70672589ea30bb9540c6c9d3121d3a9b
Stephen Marold <smmarold@email.neit.edu>
2021-10-13 18:24:01 -0400

    Unfinished Intake Form v2

---
+++ SE266/portfolio_website/placeholder.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Resources</title>
+    <style>
+        .navbar{
+            background-color:#404040;
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 1.5em;
+            display: flex;
+            justify-content: space-evenly;
+            align-items: center;
+            color: white;        }
+        .navbar a {
+            padding: 15px;
+            color: white;
+        }
+
+    </style>
+</head>
+<body>
+<div class = 'navbar'>
+        <a href="./index.php">Home</a>
+        <a href="https://github.com/smmarold/neit-2021-smarold/tree/main/SE266" target="_blank">GitHub Repo</a>
+        <a href="./phpResources.php">PHP Resources</a>
+        <a href="./gitResources.php">GitHub Resources</a>
+        <a href="./hobbies.php">What I Do</a>
+    </div>
+
+    <h1>Assignment Placeholder</h1>
+    <p>Placeholder Page</p>
+    <?php include __DIR__ . './footer.php'; ?>
